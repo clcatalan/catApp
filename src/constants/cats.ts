d5d5be5462db8a9827392738b2447b8a70ae0501
Carlos Catalan <ccatalan6@gatech.edu>
2024-03-22 18:51:22 +0800

    add react bootstrap and dropdown button

---
+++ src/constants/cats.ts
@@ -0,0 +1,4 @@
+export const cats = [
+    { breed_id: 'abys', breed_name: 'Abyssinian' },
+    { breed_id: 'aege', breed_name: 'Aegean' },
+]
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+import {cats} from '../constants/cats';
 
 const Home = () => {
+
+  console.log('cats', cats);
   return (
-    <div>Home</div>
+    <Dropdown>
+      <Dropdown.Toggle variant="success" id="dropdown-basic">
+        Dropdown Button
+      </Dropdown.Toggle>
+
+      <Dropdown.Menu>
+        {
+          cats.map(c => 
+            <Dropdown.Item href="#/action-1">
+              {c.breed_name}
+            </Dropdown.Item>)
+        }
+      </Dropdown.Menu>
+    </Dropdown>
   )
 }
 
