9a9afe8a7b935b5320541b32477be2da45df155d
Carlos Catalan <ccatalan6@gatech.edu>
2024-03-28 23:46:14 +0800

    add load more functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import { Container, Dropdown } from 'react-bootstrap';
+import { useState, useEffect, useMemo } from 'react';
+import { Button, Container, Dropdown } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -19,12 +19,19 @@ const PhotosContainer = styled.div`
   display: flex;
   flex-direction: row;
   padding: 10px;
+  width: 100%;
+  flex-wrap: wrap;
 `;
+
+const PAGE_LIMIT = 10;
 
 const Home = () => {
 
+  const pageLimit = useMemo(() => 10, [PAGE_LIMIT]);
   const [photos, setPhotos] = useState<Photo[]>([]);
-  const [selectedBreed, setSelectedBreed] = useState('');
+  const [currentSelectedBreed, setCurrentSelectedBreed] = useState('');
+  const [currentPage, setCurrentPage] = useState(0);
+  const [loadMoreVisible, setLoadMoreVisible] = useState(false);
 
   const location = useLocation();
 
@@ -32,14 +39,16 @@ const Home = () => {
     if(location.state) {
       
       const { state } = location;
-      console.log('state home', state)
 
       getPhotos(state.breed_id)
     }
   }, [location]);
 
   const getPhotos = (eventKey: string) => {
-    fetch(`https://api.thecatapi.com/v1/images/search?page=1&limit=10&breed_id=${eventKey}`, {
+
+    const newPage = currentPage + 1;
+
+    fetch(`https://api.thecatapi.com/v1/images/search?page=${newPage}&limit=${pageLimit}&breed_id=${eventKey}`, {
         mode:  'cors', 
         method: 'GET',
         headers: {
@@ -48,9 +57,22 @@ const Home = () => {
       })
       .then(response => response.json())
       .then(data => {
-        console.log('data', data)
-        setSelectedBreed(eventKey);
-        setPhotos(data);
+        let combinedData = [...data];
+        if(currentSelectedBreed === eventKey) {
+          combinedData = [...photos, ...combinedData];
+        }
+        setCurrentSelectedBreed(eventKey);
+        setPhotos(combinedData);
+        setCurrentPage(newPage);
+
+
+        if(data.length < pageLimit) {
+          //if data returned is less than the page limit, then there is no more data to return
+          setLoadMoreVisible(false);
+        } else {
+          // if data returned is at least equal to the page limit, then there is a possibility of more data to load
+          setLoadMoreVisible(true);
+        }
       })
       .catch(error => console.error(error));
   }
@@ -74,10 +96,11 @@ const Home = () => {
       <PhotosContainer>
           {
             photos.map(p => 
-              <Card imgUrl={p.url} id={p.id} selectedBreed={selectedBreed}></Card>
+              <Card imgUrl={p.url} id={p.id} selectedBreed={currentSelectedBreed}></Card>
             )
           }
       </PhotosContainer>
+      {loadMoreVisible && <Button onClick={()=> getPhotos(currentSelectedBreed)}>Load More</Button>}
     </StyledContainer>
     
   )
